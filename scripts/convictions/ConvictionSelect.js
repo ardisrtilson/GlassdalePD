68ea7dd31f5e21af413c12834fb2e980e623cbe7
ardisrtilson <email@example.com>
2020-07-24 15:25:39 -0500

    Dropdown complete

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -1,14 +1,9 @@
 import { useConvictions, getConvictions } from "./ConvictionProvider.js"
 
-// Get a reference to the DOM element where the <select> will be rendered
 const contentTarget = document.querySelector(".filters__crime")
 
 const render = convictionsCollection => {
-    /*
-        Use interpolation here to invoke the map() method on
-        the convictionsCollection to generate the option elements.
-        Look back at the example provided above.
-    */
+
     contentTarget.innerHTML = `
         <select class="dropdown" id="crimeSelect">
             <option value="0">Please select a crime...</option>
@@ -25,7 +20,7 @@ const render = convictionsCollection => {
 
 export const ConvictionSelect = () => {
     getConvictions().then(() => {
-        // Get all convictions from application state
+
         const convictions = useConvictions()
 
         render(convictions)
