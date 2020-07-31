810f4b9fda83160157641a50f466dde6bcd26bcf
ardisrtilson <email@example.com>
2020-07-31 10:57:56 -0500

    Removed debugger line.

--- notes/ShowNoteButton.js
+++ notes/ShowNoteButton.js
@@ -3,7 +3,6 @@ const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("click", clickEvent => {
     if(clickEvent.target.id === "showNotes") {
-        debugger
         const customEvent = new CustomEvent("showNotesClicked")
         eventHub.dispatchEvent(customEvent)
     }
