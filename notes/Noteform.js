5cf5c95a0fb9b7d32c90c32722c91336f35ed520
ardisrtilson <email@example.com>
2020-08-13 11:28:58 -0500

    Added criminal ID selector

--- notes/Noteform.js
+++ notes/Noteform.js
@@ -1,4 +1,5 @@
 import { saveNote } from "./NoteProvider.js"
+import { useCriminals, getCriminals } from "../scripts/criminals/CriminalProvider.js"
 
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".noteFormContainer")
@@ -8,28 +9,50 @@ eventHub.addEventListener("click", clickEvent => {
         const noteTitle = document.querySelector("#note--title")
         const noteAuthor = document.querySelector("#note--author")
         const noteContent = document.querySelector("#note--content")
-    
+        const noteCriminal = document.querySelector("#noteForm--criminal")
+        const criminalId = parseInt(noteCriminal.value)
+
+        if (criminalId !== 0) {
         const newNote = {
             title: noteTitle.value,
             author: noteAuthor.value,
             content: noteContent.value,
             timestamp: Date.now(),
-            criminalId: 4
+            criminalId: parseInt(noteCriminal.value)
         }
         saveNote(newNote)
-        render()
     }
+    else {
+        window.alert("Please choose a criminal, dummy!")
+    }
+}
 })
 
-const render = () => {
+const render = (criminals) => {
     contentTarget.innerHTML = `
         <input type="text" id="note--title" placeholder ="Enter note title" />
         <input type="text" id="note--author" placeholder="Your name here" />
         <textarea id="note--content" placeholder="Note text here"></textarea>
+        <select id="noteForm--criminal">
+        <option value="0">Select a criminal...</option>
+        ${
+            criminals.map(
+                (criminalObject) => {
+                    return `<option value="${criminalObject.id}">
+                        ${ criminalObject.name }
+                    </option>`
+                }
+            )
+        }
+    </select>
         <button class="buttonSpace" id="saveNote">Save Note</button>
         </article>`
 }
 
 export const NoteForm = () => {
-    render()
-}
+        getCriminals()
+            .then(() => {
+                const criminals = useCriminals()
+                render(criminals)
+            })
+    }
